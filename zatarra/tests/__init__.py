d2a07f17321bed13b37580a72e2e765dc45490e2
Dustin Chapman <dustin.r.chapman@gmail.com>
2013-03-20 08:41:35 -0400

    add uuid gen mock to testing

--- zatarra/tests/__init__.py
+++ zatarra/tests/__init__.py
@@ -12,6 +12,7 @@
 import unittest
 
 import time
+import uuid
 
 
 class BaseTestCase(unittest.TestCase):
@@ -27,6 +28,17 @@ class BaseTestCase(unittest.TestCase):
         _time = _time_gen()
         time.time = lambda: next(_time)
 
+        def _uuid_gen():
+            num = 0
+            while True:
+                num += 1
+                yield uuid.UUID('00000000-0000-0000-0000-{:012x}'.format(num))
+
+        self._old_uuid4 = uuid.uuid4
+        _uuid4 = _uuid_gen()
+        uuid.uuid4 = lambda: next(_uuid4)
+
     def tearDown(self):
         time.time = self._old_time
+        uuid.uuid4 = self._old_uuid4
 
